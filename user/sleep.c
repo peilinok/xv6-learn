f2b510750745e5a2773cc83e0ebe152753d55c8f
sylar <peilinok@163.com>
2025-04-28 00:10:30 +0800

    lab: sleep

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+
+#include "kernel/types.h"
+#include "user/user.h" // for sleep and exit
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(2, "Usage: sleep <seconds>\n");
+        exit(1);
+    }
+
+    if (sleep(atoi(argv[1])) != 0)
+    {
+        fprintf(2, "sleep: failed to sleep\n");
+        exit(1);
+    }
+
+    exit(0);
+}
